06883a9adce9aa93f274beb77e43a090afd6ebc9
woodandmortar <moreforblack@gmail.com>
2023-09-08 16:19:03 -0500

    better

--- home.js
+++ home.js
@@ -21,6 +21,23 @@ document.addEventListener('DOMContentLoaded', function () {
   var projectsList = document.querySelector('.project-boxes');
 });
 
+function toggleToolMenu() {
+    const toolMenu = document.getElementById('toolMenu');
+    const toolMenu2 = document.getElementById('toolMenu2');
+
+    if (toolMenu && toolMenu2) {
+        const isToolMenuVisible = toolMenu.style.display !== 'none';
+        const isToolMenu2Visible = toolMenu2.style.display !== 'none';
+
+        // Toggle the display property based on its current value
+        toolMenu.style.display = isToolMenuVisible ? 'none' : 'block';
+        toolMenu2.style.display = isToolMenu2Visible ? 'none' : 'block';
+    } else {
+        console.error("One or both toolMenu elements not found!");
+    }
+}
+
+
 const bookFrame4 = document.getElementById('bookFrame4');
 bookFrame4.src = 'loader/index.html';
 
