e735d2819a1b9939fbb1f86f8ee34ff6539341b9
woodandmortar <moreforblack@gmail.com>
2023-09-09 02:45:14 -0500

    population update

--- home.js
+++ home.js
@@ -6,6 +6,26 @@ document.addEventListener('DOMContentLoaded', function () {
     modeSwitch.classList.toggle('active');
   });
 
+
+  var listView = document.querySelector('.list-view');
+  var gridView = document.querySelector('.grid-view');
+  var projectsList = document.querySelector('.project-boxes');
+
+  listView.addEventListener('click', function () {
+    gridView.classList.remove('active');
+    listView.classList.add('active');
+    projectsList.classList.remove('jsGridView');
+    projectsList.classList.add('jsListView');
+  });
+
+  gridView.addEventListener('click', function () {
+    gridView.classList.add('active');
+    listView.classList.remove('active');
+    projectsList.classList.remove('jsListView');
+    projectsList.classList.add('jsGridView');
+  });
+
+
   const messagesSection = document.querySelector('.messages-section');
   const messagesBtn = document.querySelector('.messages-btn');
   const messagesClose = document.querySelector('.messages-close');
