ff0e51e45b2c290f12c77cef82065992df95df2c
woodandmortar <144293908+LuminaFieldsNZ@users.noreply.github.com>
2023-12-19 12:53:00 -0600

    model change

--- avatar/script.js
+++ avatar/script.js
@@ -22,7 +22,7 @@ https://tympanus.net/codrops/2019/10/14/how-to-create-an-interactive-3d-characte
 
   function init() {
 
-    const MODEL_PATH = 'https://models.readyplayer.me/6581baed22554d1b538c60f5.glb';
+    const MODEL_PATH = 'https://luminafields.com/avatar/models/micheal.glb';
     const canvas = document.querySelector('#c');
     const backgroundColor = 0xf1f1f1;
 
