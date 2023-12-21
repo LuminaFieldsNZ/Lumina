72dd096212a5e1b0fc4a0e701c698b1779669d0c
woodandmortar <144293908+LuminaFieldsNZ@users.noreply.github.com>
2023-12-21 06:28:48 -0600

    avatar stable
    working

--- avatar/script.js
+++ avatar/script.js
@@ -1,300 +1,180 @@
-/* Follow the tutorial here:
-https://tympanus.net/codrops/2019/10/14/how-to-create-an-interactive-3d-character-with-three-js/
-*/
+/* --------------------------
+* THREE JS EXPERIMENT
+* Waving Character
+* -------------------------- */
 
-(function () {
-  // Set our main variables
-  let scene,
-  renderer,
+/* --------------------------
+* GENERAL VARIABLES
+* -------------------------- */
+
+let scene,
   camera,
-  model, // Our character
-  neck, // Reference to the neck bone in the skeleton
-  waist, // Reference to the waist bone in the skeleton
-  possibleAnims, // Animations found in our file
-  mixer, // THREE.js animations mixer
-  idle, // Idle, the default state our character returns to
-  clock = new THREE.Clock(), // Used for anims, which run to a clock instead of frame rate
-  currentlyAnimating = false, // Used to check whether characters neck is being used in another anim
-  raycaster = new THREE.Raycaster(), // Used to detect the click on our character
-  loaderAnim = document.getElementById('js-loader');
+  renderer,
+  controls,
+  model,
+  mixer,
+  action,
+  delta;
 
-  init();
+let clock = new THREE.Clock();
 
-  function init() {
+let waveButton = document.querySelector('.wave-button');
 
-    const MODEL_PATH = 'https://luminafields.com/micheal2.glb';
-    const canvas = document.querySelector('#c');
-    const backgroundColor = 0xf1f1f1;
-
-    // Init the scene
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(backgroundColor);
-    scene.fog = new THREE.Fog(backgroundColor, 60, 100);
-
-    // Init the renderer
-    renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-    renderer.shadowMap.enabled = true;
-    renderer.setPixelRatio(window.devicePixelRatio);
-    document.body.appendChild(renderer.domElement);
-
-    // Add a camera
-    camera = new THREE.PerspectiveCamera(
-    50,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000);
-
-    camera.position.z = 30;
-    camera.position.x = 0;
-    camera.position.y = -3;
+// Add Greensock Ticker
+gsap.ticker.add(render);
 
 
-    var loader = new THREE.GLTFLoader();
+/* --------------------------
+ * INIT
+ * -------------------------- */
 
-    loader.load(
-    MODEL_PATH,
-    function (gltf) {
-      model = gltf.scene;
-      let fileAnimations = gltf.animations;
+function init() {
 
-      model.traverse(o => {
-
-        if (o.isMesh) {
-          o.castShadow = true;
-          o.receiveShadow = true;
-        }
-        // Reference the neck and waist bones
-        if (o.isBone && o.name === 'Neck') {
-          neck = o;
-        }
-        if (o.isBone && o.name === 'Spine') {
-          waist = o;
-        }
-      });
-
-      model.scale.set(7, 7, 7);
-      model.position.y = -11;
-
-      scene.add(model);
-
-      loaderAnim.remove();
-
-      mixer = new THREE.AnimationMixer(model);
-
-      let clips = fileAnimations.filter(val => val.name !== 'idle');
-      possibleAnims = clips.map(val => {
-        let clip = THREE.AnimationClip.findByName(clips, val.name);
-
-        clip.tracks.splice(3, 3);
-        clip.tracks.splice(9, 3);
-
-        clip = mixer.clipAction(clip);
-        return clip;
-      });
+  /**
+   * Scene
+   */
+  scene = new THREE.Scene();
+  /**
+  scene.background = new THREE.Color(0xffffff);
+   */
+  scene.fog = new THREE.Fog(0x000000, 0, 16);
 
 
-      let idleAnim = THREE.AnimationClip.findByName(fileAnimations, 'idle');
+  /**
+   * Camera
+   */
 
-      idleAnim.tracks.splice(3, 3);
-      idleAnim.tracks.splice(9, 3);
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera.position.set(0, 0, 4);
+  camera.lookAt(0, 0, 0);
 
-      idle = mixer.clipAction(idleAnim);
-      idle.play();
 
-    },
-    undefined, // We don't need this function
-    function (error) {
-      console.error(error);
+  /**
+   * Ambient Light
+   */
+
+  // Ambient Light
+  ambient = new THREE.AmbientLight(0xffffff, 1);
+  scene.add(ambient);
+
+
+  /**
+   * POINT LIGHTS
+   */
+
+  pointLight = new THREE.PointLight(0xffffff, 0.5);
+  pointLight.position.z = 2500;
+  scene.add(pointLight);
+
+  var pointLight2 = new THREE.PointLight(0xffffff, 1);
+  camera.add(pointLight2);
+
+  var pointLight3 = new THREE.PointLight(0xffffff, 0.5);
+  pointLight3.position.x = - 1000;
+  pointLight3.position.z = 1000;
+  scene.add(pointLight3);
+
+
+  /**
+   * Load Character
+   */
+
+  var loader = new THREE.GLTFLoader();
+
+  loader.load('https://luminafields.com/micheal2.glb', function (gltf) {
+
+    model = gltf.scene;
+    model.traverse(o => {
+      if (o.isMesh) {
+        o.castShadow = true;
+        o.receiveShadow = true;
+      }
     });
 
-
-    // Add lights
-    let hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
-    hemiLight.position.set(0, 50, 0);
-    // Add hemisphere light to scene
-    scene.add(hemiLight);
-
-    let d = 8.25;
-    let dirLight = new THREE.DirectionalLight(0xffffff, 0.54);
-    dirLight.position.set(-8, 12, 8);
-    dirLight.castShadow = true;
-    dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
-    dirLight.shadow.camera.near = 0.1;
-    dirLight.shadow.camera.far = 1500;
-    dirLight.shadow.camera.left = d * -1;
-    dirLight.shadow.camera.right = d;
-    dirLight.shadow.camera.top = d;
-    dirLight.shadow.camera.bottom = d * -1;
-    // Add directional Light to scene
-    scene.add(dirLight);
+    scene.add(model);
+    model.position.y = -0.7
 
 
-    // Floor
-    let floorGeometry = new THREE.PlaneGeometry(5000, 5000, 1, 1);
-    let floorMaterial = new THREE.MeshPhongMaterial({
-      color: 0xeeeeee,
-      shininess: 0 });
+    /**
+     * Load Character Animations
+     */
 
+    mixer = new THREE.AnimationMixer(model);
+    action = mixer.clipAction(gltf.animations[0]);
+    action.setLoop(THREE.LoopOnce);
+    action.play();
 
-    let floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    floor.rotation.x = -0.5 * Math.PI;
-    floor.receiveShadow = true;
-    floor.position.y = -11;
-    scene.add(floor);
+    /**
+     * Add Event Listeners For Interactivity
+     */
 
-    let geometry = new THREE.SphereGeometry(8, 32, 32);
-    let material = new THREE.MeshBasicMaterial({ color: 0xffda85 }); // 0xf2ce2e
-    let sphere = new THREE.Mesh(geometry, material);
+    document.addEventListener('mousedown', characterWave, false);
+    document.addEventListener("touchstart", characterWave, false);
 
-    sphere.position.z = -15;
-    sphere.position.y = -2.5;
-    sphere.position.x = -0.25;
-    scene.add(sphere);
-  }
+    function characterWave(event) {
+      // Total duration of the clip
+      let clipDuration = action._clip.duration;
 
-
-  function update() {
-    if (mixer) {
-      mixer.update(clock.getDelta());
-    }
-
-    if (resizeRendererToDisplaySize(renderer)) {
-      const canvas = renderer.domElement;
-      camera.aspect = canvas.clientWidth / canvas.clientHeight;
-      camera.updateProjectionMatrix();
-    }
-
-    renderer.render(scene, camera);
-    requestAnimationFrame(update);
-  }
-
-  update();
-
-  function resizeRendererToDisplaySize(renderer) {
-    const canvas = renderer.domElement;
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    let canvasPixelWidth = canvas.width / window.devicePixelRatio;
-    let canvasPixelHeight = canvas.height / window.devicePixelRatio;
-
-    const needResize =
-    canvasPixelWidth !== width || canvasPixelHeight !== height;
-    if (needResize) {
-      renderer.setSize(width, height, false);
-    }
-    return needResize;
-  }
-
-  window.addEventListener('click', e => raycast(e));
-  window.addEventListener('touchend', e => raycast(e, true));
-
-  function raycast(e, touch = false) {
-    var mouse = {};
-    if (touch) {
-      mouse.x = 2 * (e.changedTouches[0].clientX / window.innerWidth) - 1;
-      mouse.y = 1 - 2 * (e.changedTouches[0].clientY / window.innerHeight);
-    } else {
-      mouse.x = 2 * (e.clientX / window.innerWidth) - 1;
-      mouse.y = 1 - 2 * (e.clientY / window.innerHeight);
-    }
-    // update the picking ray with the camera and mouse position
-    raycaster.setFromCamera(mouse, camera);
-
-    // calculate objects intersecting the picking ray
-    var intersects = raycaster.intersectObjects(scene.children, true);
-
-    if (intersects[0]) {
-      var object = intersects[0].object;
-
-      if (object.name === 'stacy') {
-
-        if (!currentlyAnimating) {
-          currentlyAnimating = true;
-          playOnClick();
-        }
+      // Allow the action to restart a little before the end of the animation
+      if (action.time >= clipDuration  - 1.25) {
+          action.stop();
+          action.play();
       }
     }
-  }
 
-  // Get a random animation, and play it
-  function playOnClick() {
-    let anim = Math.floor(Math.random() * possibleAnims.length) + 0;
-    playModifierAnimation(idle, 0.25, possibleAnims[anim], 0.25);
-  }
+  }, undefined, function (error) {
 
+    console.error(error);
 
-  function playModifierAnimation(from, fSpeed, to, tSpeed) {
-    to.setLoop(THREE.LoopOnce);
-    to.reset();
-    to.play();
-    from.crossFadeTo(to, fSpeed, true);
-    setTimeout(function () {
-      from.enabled = true;
-      to.crossFadeTo(from, tSpeed, true);
-      currentlyAnimating = false;
-    }, to._clip.duration * 1000 - (tSpeed + fSpeed) * 1000);
-  }
-
-  document.addEventListener('mousemove', function (e) {
-    var mousecoords = getMousePos(e);
-    if (neck && waist) {
-
-      moveJoint(mousecoords, neck, 50);
-      moveJoint(mousecoords, waist, 30);
-    }
   });
 
-  function getMousePos(e) {
-    return { x: e.clientX, y: e.clientY };
-  }
 
-  function moveJoint(mouse, joint, degreeLimit) {
-    let degrees = getMouseDegrees(mouse.x, mouse.y, degreeLimit);
-    joint.rotation.y = THREE.Math.degToRad(degrees.x);
-    joint.rotation.x = THREE.Math.degToRad(degrees.y);
-  }
+  /**
+   * RENDERER
+   */
 
-  function getMouseDegrees(x, y, degreeLimit) {
-    let dx = 0,
-    dy = 0,
-    xdiff,
-    xPercentage,
-    ydiff,
-    yPercentage;
+   renderer = new THREE.WebGLRenderer({ alpha: true }); // Enable transparency
+ renderer.setSize(window.innerWidth, window.innerHeight);
+ document.body.appendChild(renderer.domElement);
 
-    let w = { x: window.innerWidth, y: window.innerHeight };
+  // Shadow maps
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-    // Left (Rotates neck left between 0 and -degreeLimit)
-    // 1. If cursor is in the left half of screen
-    if (x <= w.x / 2) {
-      // 2. Get the difference between middle of screen and cursor position
-      xdiff = w.x / 2 - x;
-      // 3. Find the percentage of that difference (percentage toward edge of screen)
-      xPercentage = xdiff / (w.x / 2) * 100;
-      // 4. Convert that to a percentage of the maximum rotation we allow for the neck
-      dx = degreeLimit * xPercentage / 100 * -1;
-    }
+  // Navigation Controls
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.keyPanSpeed = 100;
 
-    // Right (Rotates neck right between 0 and degreeLimit)
-    if (x >= w.x / 2) {
-      xdiff = x - w.x / 2;
-      xPercentage = xdiff / (w.x / 2) * 100;
-      dx = degreeLimit * xPercentage / 100;
-    }
-    // Up (Rotates neck up between 0 and -degreeLimit)
-    if (y <= w.y / 2) {
-      ydiff = w.y / 2 - y;
-      yPercentage = ydiff / (w.y / 2) * 100;
-      // Note that I cut degreeLimit in half when she looks up
-      dy = degreeLimit * 0.5 * yPercentage / 100 * -1;
-    }
-    // Down (Rotates neck down between 0 and degreeLimit)
-    if (y >= w.y / 2) {
-      ydiff = y - w.y / 2;
-      yPercentage = ydiff / (w.y / 2) * 100;
-      dy = degreeLimit * yPercentage / 100;
-    }
-    return { x: dx, y: dy };
-  }
+  controls.update();
 
-})();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // Set the renderer to render at native device ratios
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  // Add the threejs scene to the app div
+  document.getElementById("app").appendChild(renderer.domElement);
+
+
+  /* --------------------------
+  * BEGIN RENDER
+  * -------------------------- */
+  render();
+}
+
+
+/* --------------------------
+ * RENDER THE SCENE
+ * -------------------------- */
+
+function render() {
+  delta = clock.getDelta();
+
+  if (mixer != null) {
+    mixer.update(delta);
+  };
+
+  renderer.render(scene, camera);
+}
+
+init();
