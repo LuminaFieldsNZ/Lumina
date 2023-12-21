93dabf84e489430af77c3e0309df82160ca6a64f
woodandmortar <144293908+LuminaFieldsNZ@users.noreply.github.com>
2023-12-21 07:11:26 -0600

    testing multiple animation
    might break

--- avatar/script.js
+++ avatar/script.js
@@ -81,7 +81,7 @@ function init() {
 
   var loader = new THREE.GLTFLoader();
 
-  loader.load('https://luminafields.com/micheal2.glb', function (gltf) {
+  loader.load('https://luminafields.com/micheal3.glb', function (gltf) {
 
     model = gltf.scene;
     model.traverse(o => {
@@ -99,106 +99,27 @@ function init() {
      * Load Character Animations
      */
 
-    mixer = new THREE.AnimationMixer(model);
-    action = mixer.clipAction(gltf.animations[0]);
-    action.setLoop(THREE.LoopOnce);
-    action.play();
 
-    /**
-     * Add Event Listeners For Interactivity
-     */
+   mixer = new THREE.AnimationMixer(model);
+   let animations = gltf.animations; // Assuming all animations are stored in this array
+   let currentAnimationIndex = 0; // To keep track of the current animation
 
+   action = mixer.clipAction(animations[currentAnimationIndex]);
+   action.setLoop(THREE.LoopRepeat); // Set to loop forever
+   action.play();
 
-     model.traverse(o => {
+   /**
+    * Add Event Listeners For Interactivity
+    */
+   waveButton.addEventListener('click', function() {
+       // On click, switch to the next animation
+       action.stop(); // Stop the current animation
+       currentAnimationIndex = (currentAnimationIndex + 1) % animations.length; // Move to the next animation, loop back if at the end
+       action = mixer.clipAction(animations[currentAnimationIndex]);
+       action.setLoop(THREE.LoopRepeat);
+       action.play();
+   });
 
-       if (o.isMesh) {
-         o.castShadow = true;
-         o.receiveShadow = true;
-       }
-       // Reference the neck and waist bones
-       if (o.isBone && o.name === 'Neck') {
-         neck = o;
-       }
-       if (o.isBone && o.name === 'Spine') {
-         waist = o;
-       }
-     });
-
-    document.addEventListener('mousedown', characterWave, false);
-    document.addEventListener("touchstart", characterWave, false);
-
-    function characterWave(event) {
-      // Total duration of the clip
-      let clipDuration = action._clip.duration;
-
-      // Allow the action to restart a little before the end of the animation
-      if (action.time >= clipDuration  - 1.25) {
-          action.stop();
-          action.play();
-      }
-    }
-
-    document.addEventListener('mousemove', function (e) {
-      var mousecoords = getMousePos(e);
-      if (neck && waist) {
-
-        moveJoint(mousecoords, neck, 50);
-        moveJoint(mousecoords, waist, 30);
-      }
-    });
-
-    function getMousePos(e) {
-      return { x: e.clientX, y: e.clientY };
-    }
-
-    function moveJoint(mouse, joint, degreeLimit) {
-      let degrees = getMouseDegrees(mouse.x, mouse.y, degreeLimit);
-      joint.rotation.y = THREE.Math.degToRad(degrees.x);
-      joint.rotation.x = THREE.Math.degToRad(degrees.y);
-    }
-
-    function getMouseDegrees(x, y, degreeLimit) {
-      let dx = 0,
-      dy = 0,
-      xdiff,
-      xPercentage,
-      ydiff,
-      yPercentage;
-
-      let w = { x: window.innerWidth, y: window.innerHeight };
-
-      // Left (Rotates neck left between 0 and -degreeLimit)
-      // 1. If cursor is in the left half of screen
-      if (x <= w.x / 2) {
-        // 2. Get the difference between middle of screen and cursor position
-        xdiff = w.x / 2 - x;
-        // 3. Find the percentage of that difference (percentage toward edge of screen)
-        xPercentage = xdiff / (w.x / 2) * 100;
-        // 4. Convert that to a percentage of the maximum rotation we allow for the neck
-        dx = degreeLimit * xPercentage / 100 * -1;
-      }
-
-      // Right (Rotates neck right between 0 and degreeLimit)
-      if (x >= w.x / 2) {
-        xdiff = x - w.x / 2;
-        xPercentage = xdiff / (w.x / 2) * 100;
-        dx = degreeLimit * xPercentage / 100;
-      }
-      // Up (Rotates neck up between 0 and -degreeLimit)
-      if (y <= w.y / 2) {
-        ydiff = w.y / 2 - y;
-        yPercentage = ydiff / (w.y / 2) * 100;
-        // Note that I cut degreeLimit in half when she looks up
-        dy = degreeLimit * 0.5 * yPercentage / 100 * -1;
-      }
-      // Down (Rotates neck down between 0 and degreeLimit)
-      if (y >= w.y / 2) {
-        ydiff = y - w.y / 2;
-        yPercentage = ydiff / (w.y / 2) * 100;
-        dy = degreeLimit * yPercentage / 100;
-      }
-      return { x: dx, y: dy };
-    }
 
 
   }, undefined, function (error) {
