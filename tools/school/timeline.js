9fbacf54f560187e78b52d58e0908aaa8c243d2d
Lumina <luminafieldsnz@gmail.com>
2025-02-18 07:27:41 -0600

    timeline creation

---
+++ tools/school/timeline.js
@@ -0,0 +1,428 @@
+var timeline = [];
+
+timeline.push({
+    "events": [
+
+
+      {
+        "date": "December 20, 2022",
+        "title": "Felix was Born",
+        "subtitle": "Wyandotte County, Kansas City",
+        "financialLoss": 1500,
+        "description": [
+            {
+                "label": "Took over all housemaking",
+                "details": [
+                    "Told Crycella to take 3 Months off to recover and let me handle everything Felix",
+                    "Struggling with managing her brothers",
+                    "Crycella was present each and every day"
+                ]
+            },
+            {
+                "label": "Occupation",
+                "jobs": [
+                    "Stay at home Dad Full Time"
+                ],
+                "hours": "Implementing LuminaFields education platform and biometric tracking"
+            },
+            {
+                "label": "Felix: 100% Micheal guided day",
+                "details": [
+                    "Rigid schedule, integrating into our home after 3 days in hospital",
+                    "Food training, expanding diet",
+                    "Fighting with Crycella about her kicking us when she sleeps",
+                    "Total weight: 7.8 lbs"
+                ]
+            },
+            {
+                "label": "Evidence",
+                "documents": [
+                    {
+                        "name": "Exhibit A: Text History",
+                        "link": "https://link-to-text-history.com"
+                    },
+                    {
+                        "name": "Exhibit B: Video Recording",
+                        "link": "https://link-to-video-recording.com"
+                    },
+                    {
+                        "name": "Exhibit C: Transcript",
+                        "link": "https://link-to-transcript.com"
+                    }
+                ]
+            }
+        ]
+    },
+
+    {
+      "date": "December 20, 2023",
+      "title": "BrambleTwist LLC Move-in",
+      "subtitle": "Wyandotte County, Kansas City",
+      "financialLoss": 30000,
+      "description": [
+          {
+              "label": "Built Storefront",
+              "details": [
+                  "Spent all savings building Crycellas dream with the expectation of spending our life together",
+                  "Allowed her to focus on building her brand",
+                  "Paid for her education, with the expectation I'd be reimburshed"
+              ]
+          },
+          {
+            "label": "Physical Abuse",
+            "details": [
+                "Crycella slapped Felix for biting her nipple, so I suggested bottle feeding",
+                "I didn't want to elevate things, so we spoke about the future possibly of us splitting, and went with Crycella to get a petition for sole custody notarized, but I didnt file because I want Crycella in Felix's life and want to show her good faith",
+                "Agreed that if anything happened, we would never go to court because I'm more than fair"
+            ]
+        },
+          {
+              "label": "Occupation",
+              "jobs": [
+                  "Stay at home Dad, Co-Owner 49% Brambletwist LLC"
+              ],
+              "hours": "20 hours Brambletwist LLC, 15 hours Implementing LuminaFields education platform and biometric tracking"
+          },
+          {
+              "label": "Felix: 100% Micheal guided day",
+              "details": [
+                  "Rigid schedule, integrating into our new home and work environment",
+                  "Food training, expanding diet",
+                  "Fighting with Crycella about her kicking us when she sleeps",
+                  "Total weight: 18.2 lbs"
+              ]
+          },
+          {
+              "label": "Evidence",
+              "documents": [
+                  {
+                      "name": "Exhibit A: Text History",
+                      "link": "https://link-to-text-history.com"
+                  },
+                  {
+                      "name": "Exhibit B: Video Recording",
+                      "link": "https://link-to-video-recording.com"
+                  },
+                  {
+                      "name": "Exhibit C: Transcript",
+                      "link": "https://link-to-transcript.com"
+                  }
+              ]
+          }
+      ]
+  },
+
+
+
+
+        {
+            "date": "May 17, 2024",
+            "title": "Officially signed Co-lease in Branson West",
+            "subtitle": "Stone County, Branson",
+            "financialLoss": 2810,
+            "description": [
+                {
+                    "label": "Monthly Expenses covered by Micheal",
+                    "details": [
+                        "Rent (900)", "Electricity (120)", "Water (70)", "Trash (30)",
+                        "Phone (210)", "Internet (80)", "Gas (300)", "Food (900)", "Entertainment (200)"
+                    ]
+                },
+                {
+                  "label": "Abuse escalates",
+                  "details": [
+                      "Crycella details loudly in front of the kids, how my perspective means nothing and if she feels like I'm a liar, then she doesn't need to research because that's the truth",
+                      "Crycella then says her emotions are out of her control, and I'm lucky to only get abused this much"
+                  ]
+              },
+                {
+                    "label": "Occupation",
+                    "jobs": [
+                        "Decker Building and Construction",
+                        "Outlier AI",
+                        "Joe's Crab Shack"
+                    ],
+                    "hours": "50+ hours weekly to cover Crycella's debt and deposits for all accounts"
+                },
+                {
+                    "label": "Felix: 50% Micheal guided day",
+                    "details": [
+                        "Rigid schedule when Micheal was home",
+                        "Constant fighting with Crycella talking over Micheal",
+                        "Micheal cooking 60% of meals",
+                        "Providing 100% financially",
+                        "Woke up at midnight due to Crycella's night kicks",
+                        "Total weight: 22.3 lbs"
+                    ]
+                },
+                {
+                    "label": "Evidence",
+                    "documents": [
+                        {
+                            "name": "Exhibit A: Text History",
+                            "link": "https://link-to-text-history.com"
+                        },
+                        {
+                            "name": "Exhibit B: Video Recording",
+                            "link": "https://link-to-video-recording.com"
+                        },
+                        {
+                            "name": "Exhibit C: Transcript",
+                            "link": "https://link-to-transcript.com"
+                        }
+                    ]
+                }
+            ]
+        },
+
+
+        {
+          "date": "July 13, 2024",
+          "title": "Crycella moves back to Kansas City with Felix",
+          "subtitle": "Wyandotte County, Kansas City",
+          "financialLoss": 3500,
+          "description": [
+              {
+                  "label": "Parental Kidnapping and interfering with parental rights",
+                  "details": [
+                      "Crycella was unwilling to co-parent, unwilling to give consistent schedule",
+                      "Crycella made false promises of moving back, finding jobs in Branson West",
+                      "After one week, she brought Felix back, saying Felix should travel back and forth weekly making a 4 hour drive, I suggested that was rediculous",
+                      "Crycella tried to take Felix across stateline without my permission or knowledge, then called the police and refused to communicate with me scaring Felix"
+                  ]
+              },
+              {
+                "label": "Fraudulent Misrepresentation, Intentional Infliction of Emotional Distress (IIED), Economic Duress, Unconscionable Conduct",
+                "details": [
+                    "Crycella made false promises of moving back, saying she's finding jobs in Branson West",
+                    "She found her lawyer Mr. Garrett who told her she needed to keep me here for at least six (6) months for juristiction",
+                    "Knowing I was hurting financially, she lead me on for months to further hurt my financial standing",
+                ]
+            },
+              {
+                  "label": "Occupation",
+                  "jobs": [
+                      "Lost Bartending Job due to full time homemaking",
+                      "Outlier AI",
+                      "Roadie"
+                  ],
+                  "hours": "5+ hours weekly trying to manage Crycella refusing to help financially, leaving me with her half of rent, and all the debt from BrambleTwist LLC"
+              },
+              {
+                  "label": "Felix: 100% Micheal guided day",
+                  "details": [
+                      "Rigid schedule",
+                      "Constant fighting with Crycella talking over Micheal",
+                      "Micheal cooking 100% of meals",
+                      "Providing 100% financially",
+                      "Woke up at midnight due to Crycella's night kicks",
+                      "Total weight: 21.8 lbs"
+                  ]
+              },
+              {
+                  "label": "Evidence",
+                  "documents": [
+                      {
+                          "name": "Exhibit A: Text History",
+                          "link": "https://link-to-text-history.com"
+                      },
+                      {
+                          "name": "Exhibit B: Video Recording",
+                          "link": "https://link-to-video-recording.com"
+                      },
+                      {
+                          "name": "Exhibit C: Transcript",
+                          "link": "https://link-to-transcript.com"
+                      }
+                  ]
+              }
+          ]
+      },
+
+
+        {
+            "date": "November 13, 2024",
+            "title": "Officially moved back to Kansas City",
+            "subtitle": "Wyandotte County, Kansas City",
+            "financialLoss": 127,
+            "description": [
+                {
+                    "label": "Still under the impression of a relationship",
+                    "details": [
+                        "Rented hotels while deciding which house to rent close to Crycella",
+                        "Crycella was present each and every day, but still leaving after an hour each day"
+                    ]
+                },
+                {
+                    "label": "Occupation",
+                    "jobs": [
+                        "Roadie",
+                        "Outlier AI",
+                        "Tend Bartending"
+                    ],
+                    "hours": "20+ hours weekly while still managing Felix's LuminaFields learning platform"
+                },
+                {
+                    "label": "Felix: 100% Micheal guided day",
+                    "details": [
+                        "Rigid schedule, making it easy to sleep",
+                        "Potty trained",
+                        "No longer woke up at midnight",
+                        "Total weight: 25.7 lbs"
+                    ]
+                },
+                {
+                    "label": "Evidence",
+                    "documents": [
+                        {
+                            "name": "Exhibit A: Text History",
+                            "link": "https://link-to-text-history.com"
+                        },
+                        {
+                            "name": "Exhibit B: Video Recording",
+                            "link": "https://link-to-video-recording.com"
+                        },
+                        {
+                            "name": "Exhibit C: Transcript",
+                            "link": "https://link-to-transcript.com"
+                        }
+                    ]
+                }
+            ]
+        },
+
+        {
+          "date": "Feb 7th, 2025",
+          "title": "Abuse of Process",
+          "subtitle": "Wyandotte County, Kansas City",
+          "financialLoss": 3500,
+          "description": [
+              {
+                  "label": "Realized that I was getting played this entire time",
+                  "details": [
+                      "Realized that Crycella had no intention of keeping me in her or Felix's life",
+                      "Crycella told me, I can't see Felix nor can my friends or family"
+                  ]
+              },
+              {
+                "label": "Conspiracy to Commit Perjury, Fraudulent Misrepresentation, Malicious Prosecution",
+                "details": [
+                    "Crycella and Jozef agreed to provide false testimony about any eviction at 4120 Adams St",
+                    "Crycella and Jozef knowingly spread false information about my housing to manipulate the court",
+                    "Crycella and Jozef's actions in pursuing these false claims led to the initiation of court proceedings against me with no justifiable basis"
+                ]
+            },
+            {
+              "label": "Interference with Custody, Violation of Custody Rights, Coercion or Intimidation",
+              "details": [
+                  "Crycella prevents or interferes with the my legal right to spend time with their child",
+                  "Court order was in place granting you visitation, and Crycella refuses to comply with it",
+                  "Crycella is using the police or threats of gun violence to prevent you from seeing your child"
+              ]
+          },
+              {
+                  "label": "Occupation",
+                  "jobs": [
+                      "Roadie",
+                      "Outlier AI",
+                      "Tend Bartending"
+                  ],
+                  "hours": "20+ hours weekly while representing myself Pro Se and learning Family Law"
+              },
+              {
+                  "label": "Felix: 0% Micheal guided day",
+                  "details": [
+                      "Crycella Refuses tell tell me about his sleep",
+                      "Crycella Refuses tell tell me about his potty training",
+                      "Crycella Refuses tell tell me about his school",
+                      "Crycella Refuses tell tell me about his weight: unknown"
+                  ]
+              },
+              {
+                  "label": "Evidence",
+                  "documents": [
+                      {
+                          "name": "Exhibit A: Text History",
+                          "link": "https://link-to-text-history.com"
+                      },
+                      {
+                          "name": "Exhibit B: Video Recording",
+                          "link": "https://link-to-video-recording.com"
+                      },
+                      {
+                          "name": "Exhibit C: Transcript",
+                          "link": "https://link-to-transcript.com"
+                      }
+                  ]
+              }
+          ]
+      }
+
+
+    ]
+});
+
+function loadTimeline() {
+    const timelineContainer = document.getElementById('timeline');
+
+    timeline.forEach(data => {
+        data.events.forEach(event => {
+            const li = document.createElement('li');
+            li.className = 'timeline-event';
+
+            const icon = document.createElement('label');
+            icon.className = 'timeline-event-icon';
+
+            const copy = document.createElement('div');
+            copy.className = 'timeline-event-copy';
+
+            const date = document.createElement('p');
+            date.className = 'timeline-event-thumbnail';
+            date.textContent = event.date;
+
+            const financialLoss = document.createElement('p');
+            financialLoss.className = 'timeline-event-thumbnail';
+            financialLoss.style.marginLeft = '15px';
+            financialLoss.innerHTML = `Financial Loss: <font style="color: red;">$${event.financialLoss.toFixed(2)}</font>`;
+
+            const title = document.createElement('h3');
+            title.textContent = event.title;
+
+            const subtitle = document.createElement('h4');
+            subtitle.textContent = event.subtitle;
+
+            copy.appendChild(date);
+            copy.appendChild(financialLoss);
+            copy.appendChild(title);
+            copy.appendChild(subtitle);
+
+            event.description.forEach(desc => {
+                if (desc.label === "Evidence") {
+                    const evidenceSection = document.createElement('p');
+                    evidenceSection.innerHTML = "<strong>" + desc.label + "</strong><br>";
+
+                    desc.documents.forEach(doc => {
+                        const button = document.createElement('a');
+                        button.href = doc.link;
+                        button.target = "_blank";
+                        button.innerHTML = `<button>View Document: ${doc.name}</button>`;
+                        evidenceSection.appendChild(button);
+                        evidenceSection.appendChild(document.createElement('br'));
+                    });
+
+                    copy.appendChild(evidenceSection);
+                } else {
+                    const section = document.createElement('p');
+                    section.innerHTML = `<strong>${desc.label}</strong><br>${(desc.details || desc.jobs || []).join(', ')}`;
+                    copy.appendChild(section);
+                }
+            });
+
+            li.appendChild(icon);
+            li.appendChild(copy);
+            timelineContainer.appendChild(li);
+        });
+    });
+}
+
+window.onload = loadTimeline;
